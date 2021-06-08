6fd4841b6947dc435f55c6a497cb687c31c3ab4f
Matheus Carretta <matheuscarretta@gmail.com>
2021-06-08 15:12:46 -0300

    Requisito 1 concluido

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import reducer from '../reducers';
+import reducers from '../reducers';
 
 const store = createStore(
-  reducer,
+  reducers,
   composeWithDevTools(
     applyMiddleware(thunk),
   ),
