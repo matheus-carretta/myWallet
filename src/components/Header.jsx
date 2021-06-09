27a03ec1dcbbae3ca7da691e9a529201841386c7
Matheus Carretta <matheuscarretta@gmail.com>
2021-06-09 16:25:08 -0300

    Cria um header para a wallet

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends Component {
+  render() {
+    const { userEmail } = this.props;
+    return (
+      <header>
+        <p data-testid="email-field">{ userEmail }</p>
+        <span data-testid="total-field">0 </span>
+        <span data-testid="header-currency-field">BRL</span>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+Header.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
